b6ae1b33d32e9298f1b79cc81ae6cfe234cbb062
Krystof Woldrich <krystof.woldrich@sentry.io>
2025-04-02 18:18:57 +0200

    fix the module type

--- packages/lynx-plugin/src/index.ts
+++ packages/lynx-plugin/src/index.ts
@@ -1,1 +1,31 @@
-export * from '@sentry/webpack-plugin';
+import type { RsbuildPlugin } from '@rsbuild/core'
+import { sentryWebpackPlugin, type SentryWebpackPluginOptions } from '@sentry/webpack-plugin';
+
+/**
+ * Create a rsbuild plugin for Sentry Lynx SDK.
+ *
+ * @example
+ * ```ts
+ * // lynx.config.ts
+ * import { pluginSentryLynx } from '@sentry/lynx/plugin'
+ * export default {
+ *   plugins: [pluginSentryLynx({
+ *     org: 'your-organization',
+ *     project: 'your-project',
+ *     authToken: process.env.SENTRY_AUTH_TOKEN,
+ *   })],
+ * }
+ * ```
+ *
+ * @public
+ */
+export function pluginSentryLynx(options: SentryWebpackPluginOptions): RsbuildPlugin {
+  return {
+    name: 'sentry:rsbuild:lynx',
+    setup(api) {
+      api.modifyRspackConfig((config) => {
+        config.plugins?.push(sentryWebpackPlugin(options));
+      });
+    },
+  }
+}
