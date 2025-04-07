46554b734bf182977541546551270fad434a453b
Krystof Woldrich <krystof.woldrich@sentry.io>
2025-04-07 11:08:32 +0200

    add info

--- samples/react/src/App.tsx
+++ samples/react/src/App.tsx
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/lynx'
 
 Sentry.init({
   dsn: 'https://b4d4959ce2272d9de84894b1a656db59@o447951.ingest.us.sentry.io/4509089868152832',
+  // TODO: On web true causes error calling warn on undefined
   debug: true,
   beforeSend: (event, hint) => {
     console.log('beforeSend', event, hint)
